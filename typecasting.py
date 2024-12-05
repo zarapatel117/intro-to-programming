a3115e98ea91dab13d31541a773efb59f9417b7e
zarapatel117 <zarapatel2510@gmail.com~>
2024-12-05 20:47:10 +0530

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,11 @@
+k=1.234
+o=123
+n="blah blah"
+l=True
+print("type of k=",type(k))
+print("type of o=",type(o))
+print("type of n=",type(n))
+print("type of l=",type(l))
+print("datatypes after typecasting are......")
+k=int(k)
+print("type of k",type(k))
