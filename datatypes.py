bba249574d56b9bef30fbda59caba3dc6a5c5e72
zarapatel117 <zarapatel2510@gmail.com~>
2024-12-05 20:38:57 +0530

    datatypes in python

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+# creating data types using variables
+k= "meow meow"
+print("type of k", type(k))
+v= 123456789
+print("type of v", type(v))
+u= 1.234567
+print("type of u", type(u))
+b= True
+print("type of b", type(b))
